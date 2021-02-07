380f136b0ac8f0deb257f943404a294778a13439
Avilash2001 <avilashghosh2001@gmail.com>
2021-02-07 14:59:22 +0530

    some styling of home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 //Componentsand Pages
 import Home from './pages/Home'; 
-
+//Global Styling
+import GlobalStyles from './components/GlobalStyles';
 function App() {
   
   return (
     <div className="App">
+      <GlobalStyles/>
       <Home/>
     </div>
   );
