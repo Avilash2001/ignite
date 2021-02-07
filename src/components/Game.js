4fcdeaf726e04a3ab72abd719e01feed65c68d53
Avilash2001 <avilashghosh2001@gmail.com>
2021-02-07 21:54:32 +0530

    Component Transition

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,48 +1,49 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import React from "react";
 //Styling and Animation
-import styled from 'styled-components';
-import {motion} from 'framer-motion';
+import styled from "styled-components";
+import { motion } from "framer-motion";
 //Redux
-import {useDispatch,useSelector}from 'react-redux';
+import { useDispatch } from "react-redux";
 import { loadDetail } from "../actions/detailAction";
-//Image Resizer
+import { Link } from "react-router-dom";
 import { smallImage } from "../util";
 
-const Game = ({name, released, image, id}) =>{
+const Game = ({ name, released, image, id }) => {
+  const stringPathId = id.toString();
+  //Load Detail Handler
+  const dispatch = useDispatch();
+  const loadDetailHandler = () => {
+    document.body.style.overflow = "hidden";
+    dispatch(loadDetail(id));
+  };
 
-    //LoadDetails
-    const dispatch = useDispatch();
-
-    const loadDetailHandler =()=>{
-        document.body.style.overflow = 'hidden';
-        dispatch(loadDetail(id));
-        // console.log(id)
-    }
-
-    return (
-        <StyledGame onClick={loadDetailHandler}>
-            <Link to={`/game/${id}`}>
-                <h3>{name}</h3>
-                <p>{released}</p>
-                <img src={smallImage(image,640)} alt={name}/>
-            </Link>
-        </StyledGame>
-    )
+  return (
+    <StyledGame layoutId={stringPathId} onClick={loadDetailHandler}>
+      <Link to={`/game/${id}`}>
+        <motion.h3 layoutId={`title ${stringPathId}`}>{name}</motion.h3>
+        <p>{released}</p>
+        <motion.img
+          layoutId={`image ${stringPathId}`}
+          src={smallImage(image, 640)}
+          alt={name}
+        />
+      </Link>
+    </StyledGame>
+  );
 };
 
 const StyledGame = styled(motion.div)`
-    min-height: 30vh;
-    box-shadow: 0px 5px 20px rgba(0,0,0,0.2);
-    text-align: center;
-    border-radius: 1rem;
-    overflow: hidden;
-    img{
-        width: 100%;
-        height:40vh;
-        object-fit: cover;
-    }
-    cursor: pointer;
+  min-height: 30vh;
+  box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.2);
+  text-align: center;
+  border-radius: 1rem;
+  cursor: pointer;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 40vh;
+    object-fit: cover;
+  }
 `;
 
 export default Game;
