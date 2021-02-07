d8f46eb768331a89a12a9c3f563f8b98b84aa29e
Avilash2001 <avilashghosh2001@gmail.com>
2021-02-07 23:10:40 +0530

    Added Nav bar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React from "react";
 import { Route } from "react-router-dom";
 //Componentsand Pages
-import Home from './pages/Home'; 
+import Home from "./pages/Home";
+import Nav from "./components/Nav";
 //Global Styling
-import GlobalStyles from './components/GlobalStyles';
+import GlobalStyles from "./components/GlobalStyles";
 function App() {
-  
   return (
     <div className="App">
-      <GlobalStyles/>
-      <Route path={["/game/:id","/"]}>
-        <Home/>
+      <GlobalStyles />
+      <Nav />
+      <Route path={["/game/:id", "/"]}>
+        <Home />
       </Route>
     </div>
   );
